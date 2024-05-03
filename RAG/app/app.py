2b868ff05cd690c9c2937a8e848c45e0ff891a3d
S. Dinesh Kumar <125432987+SDineshKumar1304@users.noreply.github.com>
2024-05-03 14:53:48 +0530

    Add files via upload

---
+++ RAG/app/app.py
@@ -0,0 +1,88 @@
+import streamlit as st
+import fitz
+import io
+from PIL import Image
+from langchain_community.document_loaders import PyPDFLoader
+from langchain_text_splitters import NLTKTextSplitter
+from langchain_community.vectorstores import Chroma
+from langchain_google_genai import GoogleGenerativeAIEmbeddings
+from langchain_core.messages import HumanMessage, AIMessage, SystemMessage
+from langchain_core.prompts import ChatPromptTemplate, SystemMessagePromptTemplate, HumanMessagePromptTemplate
+from langchain_core.runnables import RunnablePassthrough
+from langchain_core.output_parsers import StrOutputParser
+from langchain_google_genai import ChatGoogleGenerativeAI
+
+def read_api_key(file_path):
+    with open(file_path, 'r') as file:
+        api_key = file.read().strip()
+    return api_key
+
+def format_docs(docs):
+    return "\n\n".join(doc.page_content for doc in docs)
+
+def display_document_preview(pdf_file):
+    pdf_document = fitz.open(stream=io.BytesIO(pdf_file.read()), filetype="pdf")
+    st.sidebar.markdown("### Document Pages")
+    for page_num in range(len(pdf_document)):
+        page = pdf_document.load_page(page_num)
+        image_bytes = page.get_pixmap().tobytes("png")
+        pil_image = Image.open(io.BytesIO(image_bytes))
+        st.sidebar.image(pil_image, caption=f"Page {page_num + 1}", use_column_width=True)
+
+st.sidebar.title("Document Preview")
+uploaded_file = st.sidebar.file_uploader("Upload document file", type=["pdf", "docx"])
+
+if uploaded_file:
+    display_document_preview(uploaded_file)
+
+st.title("RAG Document Analyser🥸🔎✨")
+
+api_key_file = "DS.Key.txt"
+api_key = read_api_key(api_key_file)
+
+loader = PyPDFLoader("Resume.pdf")
+embedding_model = GoogleGenerativeAIEmbeddings(google_api_key=api_key, model="models/embedding-001")
+
+data = loader.load_and_split()
+text_splitter = NLTKTextSplitter(chunk_size=200, chunk_overlap=200)
+chunks = text_splitter.split_documents(data)
+
+db = Chroma.from_documents(chunks, embedding_model, persist_directory="./chroma_db_")
+db.persist()
+
+# Connect to the persisted Chroma database
+db_connection = Chroma(persist_directory="./chroma_db_", embedding_function=embedding_model)
+retriever = db_connection.as_retriever(search_kwargs={"k": 1})
+
+
+chat_template = ChatPromptTemplate.from_messages([
+    SystemMessage(content="""You are a Helpful AI Bot. 
+    You take the context and question from the user. Your answer should be based on the specific context."""),
+    HumanMessagePromptTemplate.from_template("""Answer the question based on the given context.
+    Context:
+    {context}
+    
+    Question: 
+    {question}
+    
+    Answer: """)
+])
+
+# Instantiate chat model
+chat_model = ChatGoogleGenerativeAI(google_api_key=api_key, model="gemini-1.5-pro-latest")
+
+# Instantiate output parser
+output_parser = StrOutputParser()
+
+# Create RAG chain
+rag_chain = (
+    {"context": retriever | format_docs, "question": RunnablePassthrough()}
+    | chat_template
+    | chat_model
+    | output_parser
+)
+
+user_input = st.text_input("Ask a question:")
+if st.button("Search"):
+    retrieved_docs = rag_chain.invoke(user_input)
+    st.write(retrieved_docs)
